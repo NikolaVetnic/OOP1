0883685cb1cf21d065a4e8f0df29483a0b654e25
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2020-10-23 10:14:57 +0200

    Dodati zadaci sa prvih prakticnih vezbi.

---
+++ src/pv01_z02/Tim.java
@@ -0,0 +1,33 @@
+package pv01_z02;
+
+public class Tim {
+
+	private Kosarkas[] tim;
+
+	public Tim(Kosarkas[] tim) {
+		super();
+		this.tim = tim;
+	}
+
+	@Override
+	public String toString() {
+		
+		String out = "";
+		
+		for (int i = 0; i < tim.length; i++)
+			out += tim[i] + "\n";
+
+		return out;
+	}
+	
+	public String najkorisnijiIgracNaTerenu() {
+		
+		int max_idx = 0;
+		
+		for (int i = 1; i < tim.length; i++)
+			if (tim[i].getKorisnost() >= tim[max_idx].getKorisnost())
+				max_idx = i;
+		
+		return tim[max_idx].getImePrezime();
+	}
+}
