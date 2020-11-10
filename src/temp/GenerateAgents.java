ac422d34b2591d7a1535a52f177b80bfc8f29492
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2020-11-10 10:50:41 +0100

    Dodat generator agenata i kontakata za cetvrte teorijske vezbe.

---
+++ src/temp/GenerateAgents.java
@@ -0,0 +1,79 @@
+package temp;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class GenerateAgents {
+	
+	public static final String OBAZRIV = "obazriv";
+	public static final String OPUSTEN = "opusten";
+
+	public static void main(String[] args) throws IOException {
+		
+		int numAgents = 1000;
+		
+		generateAgents(numAgents);
+		generateContacts(numAgents);
+	}
+	
+	public static <T> ArrayList<T> removeDuplicates(ArrayList<T> list) { 
+  
+        // Create a new LinkedHashSet 
+        Set<T> set = new LinkedHashSet<>(); 
+  
+        // Add the elements to set 
+        set.addAll(list); 
+  
+        // Clear the list 
+        list.clear(); 
+  
+        // add the elements of set 
+        // with no duplicates to the list 
+        list.addAll(set); 
+  
+        // return the list 
+        return list; 
+    }
+	
+	public static void generateAgents(int numAgents) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(
+				new FileWriter("res//agenti" + numAgents + ".txt"));
+		
+		bw.write(numAgents + "\n");
+		
+		for (int i = 0; i < numAgents; i++) {
+			bw.write((Math.random() < .5 ? OBAZRIV : OPUSTEN) + ", " + i + "\n");
+		}
+		
+		bw.close();
+	}
+	
+	public static void generateContacts(int numAgents) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(
+				new FileWriter("res//kontakti" + numAgents + ".txt"));
+		
+		for (int i = 0; i < numAgents; i++) {
+			
+			int numCont = (int) (Math.random() * numAgents);
+				numCont = numCont == 0 ? 1 : numCont;
+				
+			ArrayList<Integer> arrCont = new ArrayList<>();
+			
+			for (int j = 0; j < numCont; j++)
+				arrCont.add((int) (Math.random() * numAgents));
+			
+			arrCont = removeDuplicates(arrCont);
+			
+			for (int j = 0; j < arrCont.size(); j++)
+				bw.write(i + ", " + arrCont.get(j) + "\n");
+		}
+		
+		bw.close();
+	}
+}
