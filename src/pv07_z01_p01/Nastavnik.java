be7da54275c24590e4cc2f20fe818b17a8992740
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2020-12-14 13:33:58 +0100

    Sedmi domaci zadatak (Juice Bar).

---
+++ src/pv07_z01_p01/Nastavnik.java
@@ -0,0 +1,7 @@
+package pv07_z01_p01;
+
+public interface Nastavnik {
+
+	void odrziRoditeljski();
+	void pregledajZadatke(String predmet) throws NijePredatDomaci, FaliZadatak;
+}
---
+++ src/pv07_z01_p01/NijePredatDomaci.java
@@ -0,0 +1,13 @@
+package pv07_z01_p01;
+
+public class NijePredatDomaci extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NijePredatDomaci(String imeUcenika, String predmet) {
+		super(imeUcenika + " : fali domaci iz predmeta -> " + predmet);
+	}
+}
